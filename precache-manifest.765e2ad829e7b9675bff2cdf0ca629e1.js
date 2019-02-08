f815a375b6656c0b89ff3df142b5f3e1b564a4bf
Ricardo Mallols <ricardo.mallols@gmail.com>
2019-02-08 11:56:48 +0000

    Updates

---
+++ precache-manifest.765e2ad829e7b9675bff2cdf0ca629e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "637e82e40ec2128bccd06109ffde49d2",
+    "url": "/8-days-a-week/static/media/hourcode2016_girllllllllll.637e82e4.png"
+  },
+  {
+    "revision": "edff7cdd14afdc5d8fa2",
+    "url": "/8-days-a-week/static/js/runtime~main.edff7cdd.js"
+  },
+  {
+    "revision": "b48583e921916dfc5200",
+    "url": "/8-days-a-week/static/js/main.b48583e9.chunk.js"
+  },
+  {
+    "revision": "d76791b0b08a91871f4e",
+    "url": "/8-days-a-week/static/js/1.d76791b0.chunk.js"
+  },
+  {
+    "revision": "b48583e921916dfc5200",
+    "url": "/8-days-a-week/static/css/main.6cb7bd9f.chunk.css"
+  },
+  {
+    "revision": "d76791b0b08a91871f4e",
+    "url": "/8-days-a-week/static/css/1.31f98007.chunk.css"
+  },
+  {
+    "revision": "3cac0557897ae99dfd630c3b41667d6c",
+    "url": "/8-days-a-week/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/8-days-a-week/precache-manifest.765e2ad829e7b9675bff2cdf0ca629e1.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/8-days-a-week/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
