ee3e37dd8c9f7a508f04a7a448d82b31f6cfd451
Michael Gill <michaelgill@Omaewamhinderiu.localdomain>
2019-02-14 11:57:33 +0000

    added attributed quotes to days of the week banners , as part of #3

--- src/frontend/timeline/timeline.js
+++ src/frontend/timeline/timeline.js
@@ -11,98 +11,104 @@ class TimeLine extends Component {
     render() {
         return (
             <div className="timeLine">
-                
-                    <VerticalTimeline>
 
-                        <VerticalTimelineElement
-                            className="vertical-timeline-element--work"
-                            date="Monday"
-                            iconStyle={{ background: 'rgb(25, 149, 173)', color: '#fff' }}>
-                            <h3 className="vertical-timeline-element-title">Monday</h3>
-                            <h4 className="vertical-timeline-element-subtitle">Code Mood</h4>
-                            <p>“You can’t wait for inspiration. You have to go after it with a club.”</p>
-                            <Link to="/details" component={Details}>
-                                <button className="myButton">Click Me</button>
-                            </Link>
-                        </VerticalTimelineElement>
-                        <VerticalTimelineElement
-                            className="vertical-timeline-element--work"
-                            date="Tuesday"
-                            iconStyle={{ background: 'rgb(25, 149, 173)', color: '#fff' }}>
-                            <h3 className="vertical-timeline-element-title">Tuesday</h3>
-                            <h4 className="vertical-timeline-element-subtitle">Drink Mood</h4>
-                            <p>“If you think you are too small to make a difference, try sleeping with a mosquito.“</p>
-                            <Link to="/details" component={Details}>
-                            <button className="myButton" >Click Me</button>
-                            </Link>
-                        </VerticalTimelineElement>
-                        <VerticalTimelineElement
-                            className="vertical-timeline-element--work"
-                            date="Wednesday"
-                            iconStyle={{ background: 'rgb(25, 149, 173)', color: '#fff' }}>
-                            <h3 className="vertical-timeline-element-title">Wednesday</h3>
-                            <h4 className="vertical-timeline-element-subtitle">Learn Mood</h4>
-                            <p>“Consider the postage stamp: its usefulness consists in the ability to stick to one thing ’til it gets there.“</p>
-                            <Link to="/details" component={Details}>
-                            <button className="myButton">Click Me</button>
-                            </Link>
-                        </VerticalTimelineElement>
-                        <VerticalTimelineElement
-                            className="vertical-timeline-element--work"
-                            date="Thursday"
-                            iconStyle={{ background: 'rgb(25, 149, 173)', color: '#fff' }}>
-                            <h3 className="vertical-timeline-element-title">Thursday</h3>
-                            <h4 className="vertical-timeline-element-subtitle">Code Mood</h4>
-                            <p>“A diamond is merely a lump of coal that did well under pressure.“</p>
-                            <Link to="/details" component={Details}>
-                            <button className="myButton" >Click Me</button>
-                            </Link>
-                        </VerticalTimelineElement>
-                        <VerticalTimelineElement
-                            className="vertical-timeline-element--education"
-                            date="Friday"
-                            iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}>
-                            <h3 className="vertical-timeline-element-title">Friday</h3>
-                            <h4 className="vertical-timeline-element-subtitle">Hard Drink Mood</h4>
-                            <p>“I have not failed. I’ve just found 10,000 ways that won’t work.“</p>
-                            <Link to="/details" component={Details}>
-                            <button className="myButton" >Click Me</button>
-                            </Link>
-                        </VerticalTimelineElement>
-                        <VerticalTimelineElement
-                            className="vertical-timeline-element--education"
-                            date="Saturday"
-                            iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}>
-                            <h3 className="vertical-timeline-element-title">Saturday</h3>
-                            <h4 className="vertical-timeline-element-subtitle">Worse Mood</h4>
-                            <p>“Listen, smile, agree, and then do whatever you were gonna do anyway.“</p>
-                            <Link to="/details" component={Details}>
-                            <button className="myButton" >Click Me</button>
-                            </Link>
-                        </VerticalTimelineElement>
-                        <VerticalTimelineElement
-                            className="vertical-timeline-element--education"
-                            date="Sunday"
-                            iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}>
-                            <h3 className="vertical-timeline-element-title">Sunday</h3>
-                            <h4 className="vertical-timeline-element-subtitle">Relax</h4>
-                            <p>“Nothing is impossible, the word itself says ‘I’m possible’!“</p>
-                            <Link to="/details" component={Details}>
-                            <button className="myButton" >Click Me</button>
-                            </Link>
-                        </VerticalTimelineElement>
-                        <VerticalTimelineElement
-                            className="vertical-timeline-element--education"
-                            date="One More Sunday"
-                            iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}>
-                            <h3 className="vertical-timeline-element-title">One More Sunday</h3>
-                            <h4 className="vertical-timeline-element-subtitle">Relax</h4>
-                            <p>“Nothing is impossible, the word itself says ‘I’m possible’!“</p>
-                            <Link to="/details" component={Details}>
-                            <button className="myButton" >Click Me</button>
-                            </Link>
-                        </VerticalTimelineElement>
-                    </VerticalTimeline>
+                <VerticalTimeline>
+
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        date="Monday"
+                        iconStyle={{ background: 'rgb(25, 149, 173)', color: '#fff' }}>
+                        <h3 className="vertical-timeline-element-title">Monday</h3>
+
+                        <p>"The Computer was the first machine man built that assisted the power of his brain instead of the strength of his arm"</p>
+                        <h6>Grace Hopper : Computer pioneer</h6>
+                        <Link to="/details" component={Details}>
+
+                        </Link>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        date="Tuesday"
+                        iconStyle={{ background: 'rgb(25, 149, 173)', color: '#fff' }}>
+                        <h3 className="vertical-timeline-element-title">Tuesday</h3>
+
+                        <p>"We build our computer (systems) the way we build our cities: over time, without a plan, on top of ruins"</p>
+                        <h6>Ellen Ullman : Software engineer and author</h6>
+                        <Link to="/details" component={Details}>
+
+                        </Link>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        date="Wednesday"
+                        iconStyle={{ background: 'rgb(25, 149, 173)', color: '#fff' }}>
+                        <h3 className="vertical-timeline-element-title">Wednesday</h3>
+                        <p>"We hold ourselves back in ways both big and small, by lacking self-confidence, by not raising our hands, and by pulling back when we should be leaning in".</p>
+                        <h6>Sheryl Sandberg, Facebook COO</h6>
+
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        date="Thursday"
+                        iconStyle={{ background: 'rgb(25, 149, 173)', color: '#fff' }}>
+                        <h3 className="vertical-timeline-element-title">Thursday</h3>
+
+                        <p>“Understand well as I may, my comprehension can only be an infinitesimal fraction of all I want to understand."</p>
+                        <h6>Ada Lovelace : 'The Mother of programming'</h6>
+                        <Link to="/details" component={Details}>
+
+                        </Link>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--education"
+                        date="Friday"
+                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}>
+                        <h3 className="vertical-timeline-element-title">Friday</h3>
+
+                        <p>“When I first started using the phrase software engineering, it was considered to be quite amusing. They used to kid me about my radical ideas. Software eventually and necessarily gained the same respect as any other discipline.”</p>
+                        <h6>Margaret Hamilton : Mathematician and pioneering computer scientist
+                             who coined the phrase "Software Engineering"</h6>
+
+
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--education"
+                        date="Saturday"
+                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}>
+                        <h3 className="vertical-timeline-element-title">Saturday</h3>
+
+                        <p>"Life doesn’t always present you with the perfect opportunity at the perfect time. Opportunities come when you least expect them, or when you’re not ready for them. Rarely are opportunities presented to you in the perfect way, in a nice little box with a yellow bow on top. … Opportunities, the good ones, they’re messy and confusing and hard to recognize. They’re risky. They challenge you.”</p>
+                        <h6>Susan Wojcicki : CEO YouTube</h6>
+                        <Link to="/details" component={Details}>
+
+                        </Link>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--education"
+                        date="Sunday"
+                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}>
+                        <h3 className="vertical-timeline-element-title">Sunday</h3>
+
+
+                        <p>“I learned to always take on things I’d never done before. Growth and comfort do not coexist.”</p>
+                        <h6>Virginia Marie "Ginni" Rometty : Chairwoman, President and CEO of IBM</h6>
+                        <Link to="/details" component={Details}>
+
+                        </Link>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--education"
+                        date="One More Sunday"
+                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}>
+                        <h3 className="vertical-timeline-element-title">'the eighth day'</h3>
+
+                        <p>"Women belong in all places where decisions are being made... It shouldn't be that women are the exception."</p>
+                        <h6>Ruth Bader Ginsburg, associate justice of the U.S. Supreme Court</h6>
+                        <Link to="/details" component={Details}>
+
+                        </Link>
+                    </VerticalTimelineElement>
+                </VerticalTimeline>
             </div>
         );
     }
