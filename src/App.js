d690946add1c4cb5c30ae73f35154a6724201fe7
Aysel Rzayeva <rzayeva.aysel85@gmail.com>
2019-02-21 15:19:58 +0000

    HashRouter added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import TimeLine from './frontend/timeline/timeline';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import Details from './frontend/Details/Details.js';
 
 
@@ -11,13 +11,13 @@ class App extends Component {
 
     render() {
         return (
-            <Router>
+            <HashRouter>
                 <div className="App">
                     <Route exact path="/" component={TimeLine} />
                     <Route exact path="/details" component={Details} />
                     
                 </div>
-            </Router>
+            </HashRouter>
         );
     }
 }
