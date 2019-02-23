db1f634c51b3e7d2547c6c51f1351d9efa4774ea
richard.ball <rich@MBP.local>
2019-02-23 11:18:01 +0000

    #19 created data source and dynamic react component

---
+++ src/frontend/getDays/getDays.js
@@ -0,0 +1,268 @@
+export default function getDays() {
+  return [
+    {
+      id: 'monday',
+      food: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      quote: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      nutrition: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      influencer: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      bio: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      }
+    },
+    {
+      id: 'tuesday',
+      food: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      quote: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      nutrition: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      influencer: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      bio: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      }
+    },
+    {
+      id: 'wednesday',
+      food: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      quote: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      nutrition: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      influencer: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      bio: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      }
+    },
+    {
+      id: 'thursday',
+      food: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      quote: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      nutrition: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      influencer: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      bio: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      }
+    },
+    {
+      id: 'friday',
+      food: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      quote: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      nutrition: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      influencer: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      bio: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      }
+    },
+    {
+      id: 'saturday',
+      food: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      quote: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      nutrition: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      influencer: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      bio: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      }
+    },
+    {
+      id: 'sunday',
+      food: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      quote: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      nutrition: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      influencer: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      bio: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      }
+    },
+    {
+      id: 'eighth',
+      food: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      quote: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      nutrition: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      influencer: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      },
+      bio: {
+        name: "Creamy Garlic Hummus",
+        picture: require('./food/hummus.jpg'),
+        alt: "hummus",
+        url: "https://www.delish.com/cooking/recipe-ideas/a20089167/best-homemade-hummus-recipe/"
+      }
+    }
+  ]
+}
